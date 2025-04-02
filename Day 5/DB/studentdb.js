c09cef8e9f88cda2440d68f0582a1cce37187ea2
hadhi419 <dev.hadhi419@gmail.com>
2025-04-02 06:38:41 +0530

    Day 5

---
+++ Day 5/DB/studentdb.js
@@ -0,0 +1,9 @@
+let students = [
+    { Reg_No: "2021ict109", name: "Shama", course: "IT", gender: "Female" },
+    { Reg_No: "2021ict100", name: "Rama", course: "Bio", gender: "Female" },
+    { Reg_No: "2021ict45", name: "Chuma", course: "Math", gender: "Male" },
+    { Reg_No: "2021ict89", name: "Kumar", course: "IT", gender: "Male" },
+    { Reg_No: "2021ict79", name: "Raj", course: "Math", gender: "Male" }
+];
+
+module.exports = students;
